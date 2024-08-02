54e70a6c8e9066903fb5ae75fd422a613cb3781d
Santhosh S <santhosh.senthil552003@gmail.com>
2024-08-02 17:01:39 +0530

    include: google site verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import { About, Contact, Hero, Projects, Stack, Footer } from '@/components'
 import { Navbar } from '@/components/navbar'
 
 import { GoogleAnalytics } from 'nextjs-google-analytics'
+import Head from 'next/head'
 
 export default function Home () {
   return (
     <main className='h-full w-full flex flex-col px-5 py-5 font-poppins bg-snow dark:bg-main text-alternate dark:text-snow duration-500 ease-out'>
+      <Head>
+        <meta name="google-site-verification" content="3WO8rexS4qJlr75rtIHE96Z7pm6xokw7sCWMexITkK4" />
+        <title>Home Page</title>
+      </Head>
       <GoogleAnalytics gaMeasurementId="G-05R6J9WD95" />
       <Navbar />
       <Hero />
